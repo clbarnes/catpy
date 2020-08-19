64037739c9d4bc72d969a552dcd11646a9f9a9a5
Chris Barnes <barnesc@janelia.hhmi.org>
2020-08-19 13:24:40 +0100

    next_version can supply current

--- next_version.py
+++ next_version.py
@@ -2,28 +2,52 @@
 import datetime as dt
 import runpy
 from pathlib import Path
-import sys
 import warnings
+from argparse import ArgumentParser
 
-proj_root = Path(__file__).resolve().parent
-version_path = proj_root / "catpy" / "version.py"
 
-old_str = runpy.run_path(version_path)["__version__"]
+def get_current() -> str:
+    proj_root = Path(__file__).resolve().parent
+    version_path = proj_root / "catpy" / "version.py"
 
-new_str = dt.date.today().strftime("%Y.%m.%d")
+    return runpy.run_path(version_path)["__version__"]
 
-if not old_str.startswith(new_str):
-    print(new_str)
-    sys.exit()
 
-trailing = old_str.split(".")[3:]
-if trailing:
-    suffix = int(trailing[0]) + 1
-    if suffix > 9:
-        warnings.warn(
-            f"Sub-day release ('{suffix}') has >1 digit, which will break version ordering."
-        )
-else:
-    suffix = 1
+def get_next(current) -> str:
+    new_str = dt.date.today().strftime("%Y.%m.%d")
 
-print(f"{new_str}.{suffix}")
+    if not current.startswith(new_str):
+        return new_str
+
+    trailing = current.split(".")[3:]
+    if trailing:
+        suffix = int(trailing[0]) + 1
+        if suffix > 9:
+            warnings.warn(
+                f"Sub-day release ('{suffix}') has >1 digit, which will break version ordering."
+            )
+    else:
+        suffix = 1
+
+    return f"{new_str}.{suffix}"
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-v", action="store_true", help="prefix with 'v'")
+    parser.add_argument(
+        "-c", "--current",
+        action="store_true",
+        help="return the current, rather than the next, version"
+    )
+    parsed = parser.parse_args()
+    v = "v" if parsed.v else ""
+    current = get_current()
+    if parsed.current:
+        print(v + current)
+    else:
+        print(v + get_next(current))
+
+
+if __name__ == "__main__":
+    main()
